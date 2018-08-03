c1c6f1ce1c6e0401ae96d1b1a5d2e3cc35bef24a
brian correa <brianrafael0313@hotmail.com>
2018-08-03 01:58:26 -0400

    .

---
+++ SistemaMatriculacion/SistemaMatriculacion/Controllers/estudianteController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using SistemaMatriculacion.Models;
+
+namespace SistemaMatriculacion.Controllers
+{
+    public class estudianteController : Controller
+    {
+        private Sistema_matriculacionEntities db = new Sistema_matriculacionEntities();
+
+        // GET: estudiante
+        public ActionResult Index()
+        {
+            return View(db.ESTUDIANTE.ToList());
+        }
+
+        // GET: estudiante/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // GET: estudiante/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: estudiante/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MATRICULA,NOMBRE_ESTUDIANTE,APELLIDO_ESTUDIANTE,FECHA_NACIMIENTO_ESTUDIANTE,SEXO_ESTUDIANTE,DIRECCION_ESTUDIANTE")] ESTUDIANTE eSTUDIANTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ESTUDIANTE.Add(eSTUDIANTE);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(eSTUDIANTE);
+        }
+
+        // GET: estudiante/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // POST: estudiante/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MATRICULA,NOMBRE_ESTUDIANTE,APELLIDO_ESTUDIANTE,FECHA_NACIMIENTO_ESTUDIANTE,SEXO_ESTUDIANTE,DIRECCION_ESTUDIANTE")] ESTUDIANTE eSTUDIANTE)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eSTUDIANTE).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // GET: estudiante/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            if (eSTUDIANTE == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eSTUDIANTE);
+        }
+
+        // POST: estudiante/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
+            db.ESTUDIANTE.Remove(eSTUDIANTE);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
