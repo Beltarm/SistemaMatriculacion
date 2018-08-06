31eb0e0204ea0eaa7359df90fdb207dd89da7d4e
brian correa <brianrafael0313@hotmail.com>
2018-08-06 01:58:30 -0400

    agregado controlador estudiantes y profesor

--- SistemaMatriculacion/SistemaMatriculacion/Controllers/PROFESORController.cs
+++ SistemaMatriculacion/SistemaMatriculacion/Controllers/PROFESORController.cs
@@ -10,107 +10,107 @@ using SistemaMatriculacion.Models;
 
 namespace SistemaMatriculacion.Controllers
 {
-    public class ESTUDIANTEController : Controller
+    public class PROFESORController : Controller
     {
         private Sistema_matriculacionEntities2 db = new Sistema_matriculacionEntities2();
 
-        // GET: ESTUDIANTE
+        // GET: PROFESOR
         public ActionResult Index()
         {
-            return View(db.ESTUDIANTE.ToList());
+            return View(db.PROFESOR.ToList());
         }
 
-        // GET: ESTUDIANTE/Details/5
-        public ActionResult Details(string id)
+        // GET: PROFESOR/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
-            if (eSTUDIANTE == null)
+            PROFESOR pROFESOR = db.PROFESOR.Find(id);
+            if (pROFESOR == null)
             {
                 return HttpNotFound();
             }
-            return View(eSTUDIANTE);
+            return View(pROFESOR);
         }
 
-        // GET: ESTUDIANTE/Create
+        // GET: PROFESOR/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: ESTUDIANTE/Create
+        // POST: PROFESOR/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "MATRICULA,NOMBRE_ESTUDIANTE,APELLIDO_ESTUDIANTE,FECHA_NACIMIENTO_ESTUDIANTE,SEXO_ESTUDIANTE,DIRECCION_ESTUDIANTE")] ESTUDIANTE eSTUDIANTE)
+        public ActionResult Create([Bind(Include = "ID_PROFESOR,NOMBRE_PROFESOR,APELLIDO_PROFESOR,FECHA_NACIMIENTO_PROFESOR,SEXO_PROFESOR,DIRECCION_PROFESOR,TELEFONO_PROFESOR")] PROFESOR pROFESOR)
         {
             if (ModelState.IsValid)
             {
-                db.ESTUDIANTE.Add(eSTUDIANTE);
+                db.PROFESOR.Add(pROFESOR);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
-            return View(eSTUDIANTE);
+            return View(pROFESOR);
         }
 
-        // GET: ESTUDIANTE/Edit/5
-        public ActionResult Edit(string id)
+        // GET: PROFESOR/Edit/5
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
-            if (eSTUDIANTE == null)
+            PROFESOR pROFESOR = db.PROFESOR.Find(id);
+            if (pROFESOR == null)
             {
                 return HttpNotFound();
             }
-            return View(eSTUDIANTE);
+            return View(pROFESOR);
         }
 
-        // POST: ESTUDIANTE/Edit/5
+        // POST: PROFESOR/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "MATRICULA,NOMBRE_ESTUDIANTE,APELLIDO_ESTUDIANTE,FECHA_NACIMIENTO_ESTUDIANTE,SEXO_ESTUDIANTE,DIRECCION_ESTUDIANTE")] ESTUDIANTE eSTUDIANTE)
+        public ActionResult Edit([Bind(Include = "ID_PROFESOR,NOMBRE_PROFESOR,APELLIDO_PROFESOR,FECHA_NACIMIENTO_PROFESOR,SEXO_PROFESOR,DIRECCION_PROFESOR,TELEFONO_PROFESOR")] PROFESOR pROFESOR)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(eSTUDIANTE).State = EntityState.Modified;
+                db.Entry(pROFESOR).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(eSTUDIANTE);
+            return View(pROFESOR);
         }
 
-        // GET: ESTUDIANTE/Delete/5
-        public ActionResult Delete(string id)
+        // GET: PROFESOR/Delete/5
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
-            if (eSTUDIANTE == null)
+            PROFESOR pROFESOR = db.PROFESOR.Find(id);
+            if (pROFESOR == null)
             {
                 return HttpNotFound();
             }
-            return View(eSTUDIANTE);
+            return View(pROFESOR);
         }
 
-        // POST: ESTUDIANTE/Delete/5
+        // POST: PROFESOR/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            ESTUDIANTE eSTUDIANTE = db.ESTUDIANTE.Find(id);
-            db.ESTUDIANTE.Remove(eSTUDIANTE);
+            PROFESOR pROFESOR = db.PROFESOR.Find(id);
+            db.PROFESOR.Remove(pROFESOR);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
