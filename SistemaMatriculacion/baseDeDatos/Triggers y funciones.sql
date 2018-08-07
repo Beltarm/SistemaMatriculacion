d5f563bc20c83ba7a44be680d0d4e722ce2afee6
ALEXANDER BATISTA REYES <20170393@unapec.edu.do>
2018-08-07 14:18:33 -0400

    Agregando trigger 1

---
+++ SistemaMatriculacion/baseDeDatos/Triggers y funciones.sql
@@ -0,0 +1,15 @@
+﻿ USE Sistema_matriculacion
+
+                GO
+	             
+				CREATE TRIGGER cantidad_estudiantes
+				ON ESTUDIANTE_SECCION
+				For INSERT 
+				AS
+				UPDATE dbo.SECCION
+				SET CANTIDAD_ESTUDIANTES= CANTIDAD_ESTUDIANTES + 1
+				FROM dbo.SECCION INNER JOIN Inserted
+				ON  dbo.SECCION.ID_SECCION =  Inserted.ID_SECCION
+
+				GO
+
